43bd2df0de2ae3f89280961dcb4a86689456c835
aendter <aendter83@gmail.com>
2024-03-11 22:09:39 +0100

    Remove deprecated stuff

--- src/test/java/alex/jenkins/plugins/FileSystemListParameterDefinitionTest.java
+++ src/test/java/alex/jenkins/plugins/FileSystemListParameterDefinitionTest.java
@@ -4,18 +4,14 @@ import hudson.model.FreeStyleProject;
 import hudson.model.ParametersDefinitionProperty;
 import org.junit.Rule;
 import org.junit.Test;
-import org.jvnet.hudson.test.For;
-import org.jvnet.hudson.test.Issue;
 import org.jvnet.hudson.test.JenkinsRule;
 
-@For(FileSystemListParameterDefinition.class)
 public class FileSystemListParameterDefinitionTest {
 
     @Rule
     public JenkinsRule j = new JenkinsRule();
 
     @Test
-    @Issue("JENKINS-49649")
     public void smokeRoundtrip() throws Exception {
         FreeStyleProject p = j.createFreeStyleProject();
         FileSystemListParameterDefinition d = new FileSystemListParameterDefinition("name", "description", "master", "path", "", "FILE","SINGLE_SELECT", "", "", true, false, false);
