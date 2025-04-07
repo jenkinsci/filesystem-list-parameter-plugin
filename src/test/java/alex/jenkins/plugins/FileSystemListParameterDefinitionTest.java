57af552d268e2519a99ed25c69cc975b41e22b67
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2025-04-07 15:52:43 +0200

    Migrate tests to JUnit5
    * Migrate annotations and imports
    * Migrate assertions
    * Remove public visibility for test classes and methods
    * Minor code cleanup

--- src/test/java/alex/jenkins/plugins/FileSystemListParameterDefinitionTest.java
+++ src/test/java/alex/jenkins/plugins/FileSystemListParameterDefinitionTest.java
@@ -2,27 +2,24 @@ package alex.jenkins.plugins;
 
 import hudson.model.FreeStyleProject;
 import hudson.model.ParametersDefinitionProperty;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.jvnet.hudson.test.For;
 import org.jvnet.hudson.test.Issue;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
 
 @For(FileSystemListParameterDefinition.class)
-public class FileSystemListParameterDefinitionTest {
-
-    @Rule
-    public JenkinsRule j = new JenkinsRule();
+@WithJenkins
+class FileSystemListParameterDefinitionTest {
 
     @Test
     @Issue("JENKINS-49649")
-    public void smokeRoundtrip() throws Exception {
+    void smokeRoundtrip(JenkinsRule j) throws Exception {
         FreeStyleProject p = j.createFreeStyleProject();
-        FileSystemListParameterDefinition d = new FileSystemListParameterDefinition("name", "description", "master", "path", "", "FILE","SINGLE_SELECT", "", "", true, false, false);
+        FileSystemListParameterDefinition d = new FileSystemListParameterDefinition("name", "description", "master", "path", "", "FILE", "SINGLE_SELECT", "", "", true, false, false);
         ParametersDefinitionProperty params = new ParametersDefinitionProperty(d);
         p.addProperty(params);
 
         j.configRoundtrip(p);
     }
-
 }
